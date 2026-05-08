f23fd1745978c1a29181c39d9cb32f35c46c2025
Herdi Tr. <iam@icm.hrdtr.dev>
2026-05-08 21:11:43 +0700

    feat!: single export entrypoint

--- src/handler/index.ts
+++ src/handler/index.ts
@@ -1,13 +1,2 @@
-export type {
-  EventHandlerOptions,
-  EventHandlerLoadResult,
-  EventHandlerTrackOptions,
-  EventHandlerTrackPayload,
-  EventHandlerUnloadResult,
-  EventHandlerLoadRequestBody,
-  EventHandlerUnloadRequestBody,
-} from './event';
-export { EventHandler, createEventHandler } from './event';
-
-export type { PingHandlerOptions, PingHandlerPayload, PingHandlerResult } from './ping';
-export { PingHandler, createPingHandler, createPingResponse } from './ping';
+export * from './event';
+export * from './ping';
