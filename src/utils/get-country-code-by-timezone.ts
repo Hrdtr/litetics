dfcb6b35d36466c188fd0c8ff2716b30d72bd92f
Herdi Tr. <iam@icm.hrdtr.dev>
2026-05-08 21:20:52 +0700

    fix: inefficient country code lookup

--- src/utils/get-country-code-by-timezone.ts
+++ src/utils/get-country-code-by-timezone.ts
@@ -1,5 +1,12 @@
 import { countryTimezones } from '../data/country-timezones';
 
+const timezoneToCountry: Record<string, string> = {};
+for (const [countryCode, timezones] of Object.entries(countryTimezones)) {
+  for (const timezone of timezones) {
+    timezoneToCountry[timezone] = countryCode;
+  }
+}
+
 /**
  * Retrieves the country code based on the provided timezone.
  *
@@ -7,7 +14,5 @@ import { countryTimezones } from '../data/country-timezones';
  * @return {string | null} The country code corresponding to the timezone, or null if not found.
  */
 export const getCountryCodeByTimezone = (timezone: string): string | null => {
-  const [countryCode] =
-    Object.entries(countryTimezones).find(([, timezones]) => timezones.includes(timezone)) ?? [];
-  return countryCode || null;
+  return timezoneToCountry[timezone] ?? null;
 };
